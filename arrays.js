0bc2ff7d86ebcb9dce9ccb383a6691f8e917501f
Emily Ranson <ransonme@tcd.ie>
2018-01-19 14:44:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift("")
+  array.shift("foo")
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
